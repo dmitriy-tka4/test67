ee7674ea14305b2f6231e70bcdc885f421a18b57
Dmitriy Tkach <dmitriy.tka4@gmail.com>
2020-10-12 19:39:08 +0700

    Добавлен js

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// while
+
+let i = 0;
+
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+console.log('while done');
+
+// for
+for (let k = 0; k < 5; k++) {
+  console.log(k);
+}
+
+console.log('for done');
